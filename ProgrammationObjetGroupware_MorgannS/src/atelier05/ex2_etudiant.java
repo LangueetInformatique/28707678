7bb22a899ddfb48ecc35289ffbf79a3fb7f9353b
14005970 <isha@d219-magnetron.paris-sorbonne.fr>
2020-10-22 11:03:32 +0200

    ex2 modif

--- ProgrammationObjetGroupware_MorgannS/src/atelier05/ex2_etudiant.java
+++ ProgrammationObjetGroupware_MorgannS/src/atelier05/ex2_etudiant.java
@@ -1,13 +1,21 @@
 package atelier05;
 
 public class ex2_etudiant {
+	
+	String nom, prenom;
+	int numEtud;
+	String univ; 
+	
+	/** 
+	 * Constructeur vide 
+	 */
 
-	public ex2_etudiant() {
-		String name;
-		String numEtud;
-		String univ; 
-		
-		carteEtu = []
+	public ex2_etudiant(String nom, String prenom, int numEtud, String univ) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.numEtud = numEtud;
+		this.univ = univ;
+	
 	}
 
 }
--- ProgrammationObjetGroupware_MorgannS/src/atelier05/ex2_promotion.java
+++ ProgrammationObjetGroupware_MorgannS/src/atelier05/ex2_promotion.java
@@ -3,7 +3,9 @@ package atelier05;
 public class ex2_promotion {
 
 	public ex2_promotion() {
-		liste
+		/**
+		 * liste etudiant + prof ect 
+		 */
 	}
 
 }
