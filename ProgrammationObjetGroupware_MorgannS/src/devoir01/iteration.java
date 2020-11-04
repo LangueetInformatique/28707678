1f6ad8988f510d4ff4affa1aad682288474b3c3b
Morgann <monoc@DESKTOP-RVP8THH>
2020-11-04 15:04:11 +0100

    Devoir 1 : Iteration (TP04)

--- ProgrammationObjetGroupware_MorgannS/src/devoir01/iteration.java
+++ ProgrammationObjetGroupware_MorgannS/src/devoir01/iteration.java
@@ -3,14 +3,44 @@ package devoir01;
 public class iteration {
 
 	public iteration() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		
-		String[] prenoms = {"Christian", "Karen", "Victoria", "Agatha", "Philippe", "Djamé"};
 		int imin;
-		prenoms[0].compareTo(prenoms[1]);
+		int i;
+		int result;
+		/*
+		 * @author Morgann Sabatier 
+		 */
 	}
 
+	public static void main (String[] args) 
+	{
+		/*
+		 * @param args
+		 * 
+		 * cette classe permet d'obtenir l'indice du premier prénom dans l'ordre lexicographique
+		 * 
+		 */
+		String[] prenoms = {"Christian", "Karen", "Victoria", "Agatha", "Philippe", "Djamé"};
+		int imin = 0;
+		int i;
+		//System.out.println(prenoms[0].compareTo(prenoms[1]));
+		// Lorsque que résultat compareTo négatif --> prenoms[0] est avant dans l'ordre lexicographique
+		//
+		// Je dois vérifier si prenoms[0].compareTo(prenoms[1]) > 0
+		// Mon prénom le premier dans l'ordre lexicographique : "Agatha" soit prenoms[3]
+		
+		for (i=0; i < 5; i++) 
+		{
+			//System.out.println(prenoms[i].compareTo(prenoms[i+1]));
+			int result = prenoms[i].compareTo(prenoms[i+1]);
+			//System.out.println(result);
+			if (result < 0)
+			{	
+				imin = i;
+			}
+		}
+		System.out.println(prenoms[imin]); 	// Premier prénom ordre lexicographique
+		System.out.println(imin);			// Son indice
+	}
 }
+
+
