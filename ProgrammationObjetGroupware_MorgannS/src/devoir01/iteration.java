f269803b11f452c2bfd8669f1bc2491d9b74d4bd
Morgann <monochrome.Y.T@gmail.com>
2020-11-05 09:46:38 +0100

    Correction if dans iteration

--- ProgrammationObjetGroupware_MorgannS/src/devoir01/iteration.java
+++ ProgrammationObjetGroupware_MorgannS/src/devoir01/iteration.java
@@ -19,26 +19,27 @@ public class iteration {
 		 * cette classe permet d'obtenir l'indice du premier prénom dans l'ordre lexicographique
 		 * 
 		 */
-		String[] prenoms = {"Christian", "Karen", "Victoria", "Agatha", "Philippe", "Djamé"};
+		String[] prenoms = {"Christian", "Karen",  "Victoria", "Agatha", "Philippe", "Djamé", "Aaaaa"};
 		int imin = 0;
-		int i;
-		//System.out.println(prenoms[0].compareTo(prenoms[1]));
+		int i, longueur;
+		System.out.println(prenoms[0].compareTo(prenoms[2]));
 		// Lorsque que résultat compareTo négatif --> prenoms[0] est avant dans l'ordre lexicographique
 		//
 		// Je dois vérifier si prenoms[0].compareTo(prenoms[1]) > 0
 		// Mon prénom le premier dans l'ordre lexicographique : "Agatha" soit prenoms[3]
-		int longueur = prenoms.length;
 		
-		for (i=0; i < longueur-1; i++) 
+		longueur = prenoms.length;
+		
+		for (i=1; i < longueur; i++) 
 		{
 			//System.out.println(prenoms[i].compareTo(prenoms[i+1]));
-			int result = prenoms[i].compareTo(prenoms[i+1]);
+			int result = prenoms[imin].compareTo(prenoms[i]);
 			//System.out.println(result);
 			if (result < 0)
 			{	
+				imin = imin;
+			} else if (result > 0){
 				imin = i;
-			} else {
-				imin = i+1;
 			}
 		}
 		System.out.println("Le prénom " + prenoms[imin]); 							// Premier prénom ordre lexicographique
