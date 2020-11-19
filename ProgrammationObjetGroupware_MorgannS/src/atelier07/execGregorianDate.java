966d2a7ca8b8da66906a88068d3d3259e5cce24d
Morgann <monochrome.Y.T@gmail.com>
2020-11-19 09:46:23 +0100

    Exercice exception : division

--- ProgrammationObjetGroupware_MorgannS/src/atelier07/execGregorianDate.java
+++ ProgrammationObjetGroupware_MorgannS/src/atelier07/execGregorianDate.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 
 public class execGregorianDate {
+	
+	public static void main(String[] args) {
+
 	 String[] ids = TimeZone.getAvailableIDs(-8 * 60 * 60 * 1000);
 	 // if no ids were returned, something is wrong. get out.
 	 
@@ -13,10 +16,13 @@ public class execGregorianDate {
 	SimpleTimeZone pdt = new SimpleTimeZone(-8 * 60 * 60 * 1000, ids[0]);
 	Calendar calendar = new GregorianCalendar(pdt);
 	
-	public execGregorianDate() {
-		System.out.println("Année" + calendar.get(Calendar.YEAR));
-		System.out.println("Mois" + calendar.get(Calendar.MONTH));
-		System.out.println("Jour" + calendar.get(Calendar.DATE));
+	
+		System.out.println("Année " + calendar.get(Calendar.YEAR));
+		System.out.println("Mois " + calendar.get(Calendar.MONTH));
+		System.out.println("Jour " + calendar.get(Calendar.DATE));
+		System.out.println("Heure " + calendar.get(Calendar.HOUR));
+		System.out.println("Minutes " + calendar.get(Calendar.MINUTE));
+		System.out.println("Jour " + calendar.get(Calendar.SECOND));
 	}
 
 }
---
+++ ProgrammationObjetGroupware_MorgannS/src/atelier08/division.java
@@ -0,0 +1,20 @@
+package atelier08;
+
+public class division {
+
+	public static void main (String[] args) {
+		
+			int i = 1000, j;
+		try {
+			do 
+			{
+				i--; j = 1/i;
+			} while (true);
+	
+		} catch (Exception e) 
+	{
+			System.out.println("Il y a eu une erreur !");
+		} 
+	}
+}
+// Exception : Division par zéro 
