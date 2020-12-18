fa6ab2f291cd0a6670eabadec69ea451346cd538
Morgann <monochrome.Y.T@gmail.com>
2020-12-18 17:52:10 +0100

    Devoir 5

---
+++ ProgrammationObjetGroupware_MorgannS/src/devoir05/E421.java
@@ -0,0 +1,17 @@
+package devoir05;
+
+public class E421 extends Exception {
+
+	public E421() {
+		String nom = null;
+		try {
+			int essai = 0;
+			
+		} catch(Exception e) {
+			if(e!=null) {
+				System.out.println("Nom du lanceur : " + nom);
+			}
+		}
+	}
+
+}
---
+++ ProgrammationObjetGroupware_MorgannS/src/devoir05/division.java
@@ -0,0 +1,11 @@
+package devoir05;
+
+public class division {
+	public static void main(String[] args) {
+		int i = 1000, j;
+		do {
+			i--;
+			j = 1 / i;
+		} while (true);
+	}
+}
